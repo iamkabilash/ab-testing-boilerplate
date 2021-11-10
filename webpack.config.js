94540bfebb639ae5c1585d5d6f7f6e4534c0800d
Saabbir Hossain <thesaabbir@gmail.com>
2021-11-10 17:11:43 +0600

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,52 @@
 const path = require("path");
 const entry = require("webpack-glob-entry");
-
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const jsConfig = {
-  test: /\.m?js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: "babel-loader",
-  },
-};
+module.exports = (env, argv) => {
+  const jsConfig = {
+    test: /\.m?js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    },
+  };
 
-const cssConfig = {
-  test: /\.(scss|css)$/,
-  use: [
-    MiniCssExtractPlugin.loader,
-    "css-loader",
-    "postcss-loader",
-    {
-      loader: "sass-loader",
-      options: {
-        sassOptions: {
-          outputStyle: "expanded",
+  const cssConfig = {
+    test: /\.(scss|css)$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          sourceMap: true,
         },
       },
+      "postcss-loader",
+      {
+        loader: "sass-loader",
+        options: {
+          sourceMap: true,
+          sassOptions: {
+            outputStyle: argv.mode === "production" ? "expanded" : "compressed",
+          },
+        },
+      },
+    ],
+  };
+
+  const config = {
+    entry: entry("./src/**/*.js"),
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
     },
-  ],
-};
+    module: {
+      rules: [cssConfig],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+  };
 
-const config = {
-  entry: entry("./src/**/*.js"),
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [cssConfig],
-  },
-  plugins: [new MiniCssExtractPlugin()],
-};
-
-module.exports = (env, argv) => {
   if (argv.mode === "development") {
     config.devtool = "source-map";
   }
