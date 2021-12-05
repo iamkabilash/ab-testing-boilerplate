45060d6fc9ed9758d8d64e4df9e4d58532d64eef
Saabbir Hossain <thesaabbir@gmail.com>
2021-12-05 23:02:40 +0600

    Define some pre and post functions to run

--- src/variation-1/variation-1.js
+++ src/variation-1/variation-1.js
@@ -1,40 +1,89 @@
+// Import main Sass/SCSS here
 import "./variation-1.scss";
 
+// Variation code running alert! Remove later.
 alert("Hello from Echo AB Testing Boilerplate!");
 
 // Start Variation JS
-var TEST_NAME = {
-  init: function () {
-    // Add a test specific class to the body element
-    document.body.classList.add("test_specific_class_name");
+!(function loadVariation() {
+  // Main variation/application code
+  var test = {
+    init: function () {
+      // Add a test specific classname to the body element
+      document.body.classList.add("test_specific_class_name");
 
-    this.mainCSS();
-    this.mainJS();
-  },
-  mainCSS: function () {
-    var styleEl = document.createElement("style");
-    styleEl.setAttribute("type", "text/css");
-    document.head.appendChild(styleEl).textContent = "";
-  },
-  mainJS: function () {
-    // Get reference to the jQuery object
-    var $ = jQuery;
+      // Below function calls order is important
+      test.preSetupVariables();
+      test.preSetupEvents();
+      test.mainCSS();
+      test.mainJS();
+    },
+    preSetupVariables: function () {
+      // Setup test independent/global variables
+      test.name = "TEST_PLAN_NAME";
+      test.version = "1.0";
+    },
+    preSetupEvents: function () {
+      // Setup test independent/global events
+      document.addEventListener("click", test.handleDocumentClicks);
+      document.addEventListener("mouseout", test.handleDocumentMouseOut);
+    },
+    handleDocumentClicks: function (e) {
+      if (e.target.className.includes("ELEMENT_CLASS_NAME")) {
+        // Do something on click of element with className ELEMENT_CLASS_NAME
+      }
+    },
+    handleDocumentMouseOut: function (e) {
+      if (!e.toElement && !e.relatedTarget && e.clientY < 10) {
+        alert("Are you sure that you want to leave?");
+        document.removeEventListener("mouseout", test.handleDocumentMouseOut);
+      }
+    },
+    mainCSS: function () {
+      var styleEl = document.createElement("style");
+      styleEl.setAttribute("type", "text/css");
+      document.head.appendChild(styleEl).textContent = `
+        .test_specific_class_name * {
+          border: 2px solid red;
+        }
+      `;
+    },
+    mainJS: function () {
+      // Get jQuery reference
+      var $ = jQuery;
 
-    // Test specific JavaScript code goes here
-  },
-};
+      // Test specific JavaScript code goes here
+    },
+    postSetupVariables: function () {
+      // Setup test dependent variables
+      // test.modal = document.querySelector(".c-exit-intent-modal");
+    },
+    postSetupEvents: function () {
+      // Setup test dependent events
+      // test.modal.addEventListener("click", test.handleModalClick);
+    },
+  };
 
-!(function pollingFunction() {
-  // Return if the test already ran once.
-  if (document.querySelector(".test_specific_class_name"))
-    return "Test already ran once!";
+  // Polling conditions
+  if (
+    window.jQuery &&
+    jQuery("POLL_ELEMENT").length &&
+    !document.querySelector(".test_specific_class_name")
+  ) {
+    try {
+      // Activate test
+      test.init();
 
-  if (window.jQuery && jQuery("POLL_ELEMENT").length) {
-    // Activate test
-    TEST_NAME.init();
-
-    console.log("Test Plan: TEST_PLAN_NAME V:1.0");
+      // Success log
+      console.log(
+        "Saabbir:",
+        `${test.name}: [Variation 1]: Version ${test.version}`
+      );
+    } catch (error) {
+      // Error log
+      console.log("Saabbir:", `${test.name}: Initialization error:`, error);
+    }
   } else {
-    setTimeout(pollingFunction, 25);
+    setTimeout(loadVariation, 25);
   }
 })(); // End Variation JS
